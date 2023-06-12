705e633e1d4955b7af32fe3e3e540e10b4ee2183
Vsevolod Ovchinnikov2 <vovchinnikov@obraz.pro>
2023-06-12 12:31:04 +0300

    feat: сделать валидацию. начало

---
+++ scripts/validate.js
@@ -0,0 +1,19 @@
+// enableValidation({
+//   formSelector: '.popup__form',
+//   inputSelector: '.popup__input',
+//   submitButtonSelector: '.popup__button',
+//   inactiveButtonClass: 'popup__button_disabled',
+//   inputErrorClass: 'popup__input_type_error',
+//   errorClass: 'popup__error_visible'
+// }); 
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}); 
+
+popup__form
